75c23e6b2937d8b3335107c420b931bfafe70a4b
Idris Adetunmbi <idrisadetunmbi@icloud.com>
2025-06-14 10:07:52 +0200

    Fix typo and grammar in projects section content

--- site/src/jsMain/kotlin/dev/iad/portfoliowebsite/pagescomponents/index/sections/ProjectsSection.kt
+++ site/src/jsMain/kotlin/dev/iad/portfoliowebsite/pagescomponents/index/sections/ProjectsSection.kt
@@ -77,18 +77,18 @@ private val PROJECTS = listOf(
                 "member-retention features including the price drop notifications system (registration, notifications, " +
                 "list management, and live prices) and the favorites lists feature increasing member acquisition and engagement.\n\n" +
                 "On the tech-improvement side, I led and managed the introduction of the Jetpack Compose UI framework " +
-                "to the trivago Android app resulting in improved developer experience and faster development iterations.",
+                "to the trivago Android app, resulting in improved developer experience and faster development iterations.",
     ),
     Project(
         image = "projects/headspace-care.png",
         link = "https://play.google.com/store/apps/details?id=com.ginger",
         type = Project.Type.MOBILE,
         headline = "Headspace Care: Mental health support -- available through organizations + health plans",
-        summary = "My experience working on the Headspace Care app included handling major projects including porting the " +
+        summary = "My experience working on the Headspace Care app included handling major projects, including porting the " +
                 "Headspace Care Flutter Mobile app to a web app. This resulted in increased user base and retention " +
                 "through providing an extra platform for product access. \n\n" +
                 "I also worked in a three-person engineering sub-team to redevelop the content system, a large part of " +
-                "the app resulting, in a more scalable, higher quality of the system and in effect, a better user experience."
+                "the app, resulting in a more scalable, higher quality of the system and, in effect, a better user experience."
     ),
     Project(
         image = "projects/autochek.png",
@@ -97,7 +97,7 @@ private val PROJECTS = listOf(
         headline = "Autochek: Enjoy 360 degree solutions at your fingertips",
         summary = "I joined the Autochek startup team to develop the first release of the app handling major product features " +
                 "including ad placements/uploads, financing application process, purchase payments, user authentication, etc. " +
-                "The development featured using the Google-recommended MVVM architecture for developing Android apps, Kotlin and " +
+                "The development featured using the Google-recommended MVVM architecture for developing Android apps, Kotlin, and " +
                 "Kotlin Coroutines (for async operations).\n\n" +
                 "It is one of the projects I adopted Jetpack Compose in at the early stages " +
                 "of the framework based on my experience and understanding of declarative UI frameworks and the benefits they " +
@@ -110,7 +110,7 @@ private val PROJECTS = listOf(
         type = Project.Type.WEB,
         headline = "Aventine: THE WORLD AS YOU'LL KNOW IT",
         summary = "I worked as a solo engineer with a product designer to develop this highly-custom blogging platform built " +
-                "using Netlify CMS, ReactJS and TailwindCSS (for a top research/journalism institute). The platform featured " +
+                "using Netlify CMS, ReactJS, and TailwindCSS (for a top research/journalism institute). The platform featured " +
                 "heavy animations on the frontend using Framer Motion and some AST manipulations to develop a glossary " +
                 "database feature unique to it.",
     ),
@@ -120,7 +120,7 @@ private val PROJECTS = listOf(
         type = Project.Type.WEB,
         headline = "Mastery Transcript",
         summary = "Built with Firebase Cloud storage and ReactJS, the project started as a prototype (through rapid iterations) " +
-                "and developed till first major version of a modern transcript system for a network of high-schools in the US. It also " +
+                "and developed till the first major version of a modern transcript system for a network of high schools in the US. It also " +
                 "included highly technical, custom SVG drawings to provide an immediate visualization summary of a " +
                 "student's credit distribution.",
     ),
